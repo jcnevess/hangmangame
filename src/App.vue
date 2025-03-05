fa082878c16ce3b82fdcc10c3110ced6a7b37e07
Júlio Neves <juliocnevess@gmail.com>
2025-03-05 13:26:09 -0300

    first version of game screen

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { computed, ref } from 'vue'
 
+import IconMenu from '@/components/icons/IconMenu.vue'
+import IconHeart from './components/icons/IconHeart.vue'
+
 const alphabet = [
   'a',
   'b',
@@ -37,18 +40,13 @@ const wordToGuess = ref(
     .replace(/[\u0300-\u036f]/g, ''),
 )
 
-const guestInProgress = ref(
-  wordToGuess.value
-    .replace(/\w/g, '*')
-    .split('')
-    .map((letter) => {
-      letter === '*' ? undefined : letter
-    }),
-)
+const guestInProgress = ref(wordToGuess.value.replace(/\w/g, '*').split(''))
 
 const usedLetters = ref([])
 
 const livesLeft = ref(8)
+
+const chosenCategory = ref('cities')
 
 const wonGame = computed(() => guestInProgress.value.join('') === wordToGuess.value)
 
@@ -84,29 +82,58 @@ function isUsedLetter(letter) {
 </script>
 
 <template>
-  <main>
+  <div class="container">
+    <header>
+      <div class="header-controls">
+        <button class="menu-button" role="navigation" title="Toggle Menu">
+          <IconMenu></IconMenu>
+        </button>
+        <div class="category">{{ chosenCategory }}</div>
+      </div>
+      <div class="lives-monitor">
+        <progress
+          id="lives-meter"
+          class="lives-meter"
+          :value="livesLeft"
+          max="8"
+          title="lives left"
+        ></progress>
+        <div class="heart-icon">
+          <IconHeart></IconHeart>
+        </div>
+      </div>
+    </header>
+
     <section class="guess-screen">
-      <span
-        v-for="(letter, index) in guestInProgress"
-        :key="`${letter}-${index}`"
-        class="letter letter-slot guess-slot"
+      <div
+        v-for="(word, index) in guestInProgress.join('').split(' ')"
+        :key="`${word}-${index}`"
+        class="guess-word"
       >
-        {{ letter }}
-      </span>
+        <span
+          v-for="(letter, index) in word"
+          :key="`${letter}-${index}`"
+          class="letter guess-slot letter-slot"
+          :class="{
+            'guess-slot-empty': letter === '*',
+          }"
+          >{{ letter === '*' ? '&nbsp;' : letter }}</span
+        >
+      </div>
     </section>
 
     <section class="keyboard">
       <button
         v-for="letter in alphabet"
         :key="letter"
-        class="alphabet-letter letter-slot guess-button"
+        class="letter-slot guess-button"
         :disabled="isUsedLetter(letter) || loseGame || wonGame"
         @click="makeGuess(letter)"
       >
         {{ letter }}
       </button>
     </section>
-  </main>
+  </div>
 </template>
 
 <style scoped>
@@ -122,33 +149,121 @@ function isUsedLetter(letter) {
   text-transform: uppercase;
   font-size: 1.5rem;
   color: var(--color-dark-navy);
-  width: 28px;
-  height: 56px;
+  width: 100%;
+  aspect-ratio: 1/2;
   border: none;
 }
 
 .guess-slot {
+  width: 40px;
+  height: 66px;
+  border-radius: 12px;
   color: var(--color-white);
   background-color: var(--color-blue);
-  font-size: 2.25rem;
-  width: 36px;
-  height: 62px;
+  font-size: 2.5rem;
   box-shadow:
     inset 0 -2px 0 3px var(--color-dark-navy-shadow),
     inset 0 1px 0 6px var(--color-blue-shadow);
-  border-radius: 12px;
+}
+
+.guess-slot-empty {
+  opacity: 0.25;
 }
 
 .keyboard {
   display: grid;
   grid-template-columns: repeat(9, 1fr);
   gap: 1.5rem 0.5rem;
+  justify-items: center;
 }
 
 .guess-screen {
   display: flex;
   flex-wrap: wrap;
+  flex-grow: 1;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
   gap: 1rem 0.75rem;
+}
+
+.guess-word {
+  display: flex;
+  column-gap: 0.75rem;
+}
+
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.category {
+  color: var(--color-white);
+  text-transform: capitalize;
+  font-size: 2.5rem;
+  font-weight: 500;
+}
+
+.menu-button {
+  border-radius: 50%;
+  border: none;
+  background-image: var(--gradient-pink);
+  width: 40px;
+  height: 40px;
+}
+
+.menu-button svg {
+  width: 50%;
+}
+
+.heart-icon {
+  height: 24px;
+}
+
+.lives-monitor {
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+}
+
+progress[value] {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  border-radius: 10px;
+  background: var(--color-white);
+  padding: 0.25rem;
+  width: 62px;
+}
+
+progress[value]::-webkit-progress-bar {
+  background: var(--color-white);
+  border-radius: 10px;
+}
+
+progress[value]::-webkit-progress-value {
+  border-radius: 10px;
+  background: var(--color-dark-navy);
+}
+
+progress[value]::-moz-progress-bar {
+  background: var(--color-dark-navy);
+  border-radius: 10px;
+}
+
+.header-controls {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  height: 100%;
+  padding: 3rem 1.75rem;
+  background-color: var(--color-dark-navy-transparent);
 }
 </style>
---
+++ src/components/icons/IconMenu.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="100%"
+    height="100%"
+    fill="none"
+    viewBox="0 0 38 32"
+  >
+    <path fill="#fff" d="M0 0h38v7H0zM0 13h38v6H0zM0 25h38v7H0z" />
+  </svg>
+</template>
+
+<style scoped></style>
